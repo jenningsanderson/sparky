d92a5bc3609944b8743f9dd84716f6efd0aec231
Jennings Anderson <jennings.anderson@colorado.edu>
2015-10-16 16:43:44 -0600

    here we go

--- JSONExplorer/src/main/scala/JSONExplorer.scala
+++ JSONExplorer/src/main/scala/JSONExplorer.scala
@@ -6,46 +6,39 @@ import org.apache.spark.SparkConf
 import spray.json._
 import DefaultJsonProtocol._
 
-// case class Tweet(handle: String, time: String, text: String, context: Boolean)
-// object Tweet
+case class Tweet(handle: String, time: String, text: String, context: Boolean, geo: Array)
 
-// object MyTweetProtocol extends DefaultJsonProtocol {
-//   implicit val tweetFormat = jsonFormat4(Tweet)
-// }
-
-case class Color(name: String, red: Int, green: Int, blue: Int)
-
-object MyJsonProtocol extends DefaultJsonProtocol {
-  implicit val colorFormat = jsonFormat4(Color)
+object MyTweetProtocol extends DefaultJsonProtocol {
+  implicit val tweetFormat = jsonFormat4(Tweet)
 }
 
-import MyJsonProtocol._
+// case class Color(name: String, red: Int, green: Int, blue: Int)
 
+// object MyJsonProtocol extends DefaultJsonProtocol {
+//   implicit val colorFormat = jsonFormat4(Color)
+// }
 
-// import MyTweetProtocol._
+// import MyJsonProtocol._
+import MyTweetProtocol._
 
 object JSONExplorer {
 
   def main(args: Array[String]) {
-    val conf = new SparkConf().setAppName("JSON Explorer")
+    val conf = new SparkConf().setAppName("JSONExplorer")
     val sc = new SparkContext(conf)
-    //This is where we COULD set default JAR paths, if needed
+    // // This is where we COULD set default JAR paths, if needed
     
     val tweet_file = "/Users/jenningsanderson/Documents/sparky/spark-data/sandy_tweets.json"
     val raw_tweets = sc.textFile(tweet_file)
 
-    val json = Color("CadetBlue", 95, 158, 160).toJson; null
-    val color = json.convertTo[Color]
-
-    println(color)
-
-    // val string = raw_tweets.take(10).foreach(parseToTweet)
+    val tweet_objs = raw_tweets.map(t => parseToTweet(t))
+    
+    tweet_objs.take(10).foreach(println)
   }
 
-  // def parseToTweet(tweet: String)={
-  //   val jsonAst = tweet.parseJson
-  //   val pretty  = jsonAst.convertTo[Tweet]
-  //   //println(pretty)
-  // }
+  def parseToTweet(tweet: String)={
+    val jsonAst = tweet.parseJson
+    jsonAst.convertTo[Tweet]
+  }
 
 }
